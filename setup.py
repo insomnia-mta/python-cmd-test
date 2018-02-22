8900f44a933f39b679684e1a4a64c7b66a9d69a7
Tibor Simon <tibor@tiborsimon.io>
2018-02-23 00:22:39 +0100

    Version handling updated.

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import find_packages, setup
 import os
 
+from demo import __version__
+
 setup(
     name         = 'Imre',
-    version      = '1.0',
+    version      = __version__,
     author       = 'Tibor Simon',
     author_email = 'tibor@tiborsimon.io',
     packages     = find_packages(),
